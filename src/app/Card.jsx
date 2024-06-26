220fee11e44c42e5b5beb4ed0be22dd929eea3f0
Swayam <swayam0815@gmail.com>
2024-06-26 14:12:09 -0400

    flex wasnt working mobile

--- src/app/Card.jsx
+++ src/app/Card.jsx
@@ -6,13 +6,13 @@ function Card({heading, text, image, color }) {
     return (
       <>
         <motion.div
-          className={`flex flex-col w-full h-full p-2 sm:p-4 rounded-xl border-2 ${color} text-white text-center w-64 h-64 bg-gray-50 bg-opacity-10`}
+          className={`flex flex-col w-full h-full p-4   rounded-xl border-2 ${color} text-white text-center w-64 h-64 bg-gray-50 bg-opacity-10`}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
             
         >
-          <h2 className="text-lg font-bold px-4  lg:text-2xl">{heading}</h2>
-          <p className="text-pretty text-base p-2 mx-2 text-center">{text}</p>
+          <h2 className="text-lg font-bold lg:text-2xl my-2">{heading}</h2>
+          <p className="text-pretty text-base text-center">{text}</p>
         </motion.div>
       </>
     );
